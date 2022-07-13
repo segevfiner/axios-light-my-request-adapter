db1a07036903f29a3b2795de61fef6400995655e
Segev Finer <segev208@gmail.com>
2022-07-14 02:06:20 +0300

    Add a test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 import http from "http";
+import stream from 'stream'
 import fastify from "fastify";
 import { DispatchFunc } from "light-my-request";
 import {
@@ -41,4 +42,40 @@ test("fastify hello world", async () => {
     "content-type": "application/json; charset=utf-8",
   });
   expect(res.data).toMatchObject({ data: "Hello World!" });
+});
+
+describe('Light my Request adapter', () => {
+  const dispatch = jest.fn<void, Parameters<http.RequestListener>>();
+  let instance: Axios;
+
+  beforeEach(() => {
+    instance = axios.create({
+      baseURL: "http://localhost/",
+      adapter: createLightMyRequestAdapter(dispatch as unknown as DispatchFunc),
+    });
+  })
+
+  test('request body', async () => {
+    dispatch.mockImplementationOnce(async (req, res) => {
+      const body: {name?: string} = JSON.parse(await stream.promises.pipeline(req, async function (source) {
+        (source as stream.Readable).setEncoding('utf8');
+        const chunks = [];
+        for await (const chunk of source) {
+          chunks.push(chunk);
+        }
+        return chunks.join();
+      }));
+
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ data: `Hello ${body.name}!` }));
+    })
+
+    const res = await instance.post("/", {name: "World"});
+    expect(res.status).toBe(200);
+    expect(res.headers).toMatchObject({
+      "content-type": "application/json",
+    });
+    expect(res.data).toMatchObject({ data: "Hello World!" });
+  })
 });
