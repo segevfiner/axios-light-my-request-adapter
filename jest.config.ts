d37241d5bcea2eef3b09f41ba6bf218757810d2a
Segev Finer <segev208@gmail.com>
2025-06-04 15:48:04 +0300

    Fun

--- jest.config.ts
+++ jest.config.ts
@@ -179,7 +179,10 @@ const config: Config = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ["/node_modules/(?!(axios)/)", "\\.pnp\\.[^\\/]+$"],
+  transformIgnorePatterns: [
+    "/node_modules/\\.pnpm/(?!(axios)/)",
+    "\\.pnp\\.[^\\/]+$",
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
